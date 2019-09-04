31d0c4a7bfa0801c268ee581350279c9cae12229
mislavbreka <40965169+mislavbreka@users.noreply.github.com>
2019-09-04 21:32:14 +0200

    Add files via upload

--- domino/index.php
+++ domino/index.php
@@ -8,22 +8,18 @@
             <div class="header">
                 <div class="header0">
                     <img src="LOGO.png" class="logo" alt="">
-                </div
-                ><div class="header1">
+                </div><div class="header1">
                     <div class="search-box">
                         <input class="search-txt" type="text" name="" placeholder="Type to search">
                         <a class="search-btn" href="#" onclick="searchFunction()"> <!--search-->
                             <i class="fas fa-search"></i>
-                        </a>
-                    </div>
-                </div
-                ><div class="header2">
+                        </a></div>
+                </div><div class="header2">
                     <div class="contact-box">KONTAKT</div>
-                </div
-                ></div>
+                </div></div>
         </div>
         <div class="section1">
-            
+
         </div>
         <script src="script.js"></script>
     </body>
